5acbde198b33b8014ac3e15e3116b0f86c28fead
Kevin Lin <kevin.lin@zuhlke.com>
2023-11-26 00:40:08 +0800

    part1/01-project-setup.md

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(
+    const MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const Placeholder(),
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          background: Colors.black,
+          seedColor: Colors.red,
+          onSurface: Colors.white,
+        ),
+        textTheme: TextTheme(
+          // headlineLarge: TextStyle(color: Colors.red), // unused
+          // headlineMedium: TextStyle(color: Colors.red), // unused
+          headlineSmall: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w700,
+          ),
+          // titleLarge: TextStyle(color: Colors.red), // unused
+          titleMedium: TextStyle(color: Colors.red),
+          // titleSmall: TextStyle(color: Colors.red), // unused
+          bodyLarge: TextStyle(color: Colors.white),
+          bodyMedium: TextStyle(color: Colors.white),
+          bodySmall: TextStyle(color: Colors.white),
+          // labelLarge: TextStyle(), // unused
+          labelMedium: TextStyle(color: Colors.grey),
+          labelSmall: TextStyle(color: Colors.red),
+        ),
+        cardColor: Colors.white30,
+        iconTheme: IconThemeData(color: Colors.white),
+        datePickerTheme: DatePickerThemeData(
+          yearForegroundColor:
+          MaterialStateColor.resolveWith((states) => Colors.white),
+          yearStyle: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
