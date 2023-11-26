b51610d16a213b95c707d2bf7e00c5f4c4d84fac
Kevin Lin <kevin.linyun@gmail.com>
2023-11-27 01:44:39 +0800

    part1/12-form-finish-styling.md

--- lib/features/sign_up/sign_up_form.dart
+++ lib/features/sign_up/sign_up_form.dart
@@ -34,97 +34,112 @@ class _SignUpFormState extends State<SignUpForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: formKey,
-      child: Column(
-        children: [
-          TextFormField(
-            controller: emailTextController,
-            decoration: buildInputDecoration(context, "Email"),
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Email cannot be empty';
-              }
-              if (!value.contains("@")) {
-                return "Email need to contain @";
-              }
-              return null;
-            },
-          ),
-          TextFormField(
-            obscureText: true,
-            controller: passwordTextController,
-            decoration: buildInputDecoration(context, "Password"),
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Password cannot be empty';
-              }
-              if (value.length < 8) {
-                return 'Length of password must be more than 8 characters';
-              }
-              return null;
-            },
-          ),
-          DatePicker(
-            title: "Date of Birth",
-            controller: dateOfBirthTextController,
-          ),
-          TextFormField(
-            textAlign: TextAlign.left,
-            controller: heightTextController,
-            keyboardType: TextInputType.number,
-            inputFormatters: [
-              FilteringTextInputFormatter.allow(RegExp("[0-9.]")),
-            ],
-            decoration: buildInputDecoration(context, "Height (cm)"),
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Height cannot be empty';
-              }
-              return null;
-            },
-          ),
-          TextFormField(
-            controller: weightTextController,
-            keyboardType: TextInputType.number,
-            inputFormatters: [
-              FilteringTextInputFormatter.allow(RegExp("[0-9.]")),
-            ],
-            decoration: buildInputDecoration(context, "Weight (kg)"),
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Weight cannot be empty';
-              }
-              return null;
-            },
-          ),
-          GenderPicker(
-            selectedGender: selectedGender,
-            onChanged: (value) {
-              setState(() {
-                selectedGender = value;
-              });
-            },
-            validator: (_) {
-              if (selectedGender == null) {
-                return 'Gender must be selected';
-              }
-              return null;
-            },
-          ),
-          ElevatedButton(
-            onPressed: () {
-              FocusScope.of(context).unfocus();
-              final isFormValid = formKey.currentState?.validate() ?? false;
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: Form(
+          key: formKey,
+          child: Padding(
+            padding: const EdgeInsets.only(top: 20),
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: emailTextController,
+                  decoration: buildInputDecoration(context, "Email"),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Email cannot be empty';
+                    }
+                    if (!value.contains("@")) {
+                      return "Email need to contain @";
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 15),
+                TextFormField(
+                  obscureText: true,
+                  controller: passwordTextController,
+                  decoration: buildInputDecoration(context, "Password"),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Password cannot be empty';
+                    }
+                    if (value.length < 8) {
+                      return 'Length of password must be more than 8 characters';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 15),
+                DatePicker(
+                  title: "Date of Birth",
+                  controller: dateOfBirthTextController,
+                ),
+                SizedBox(height: 15),
+                TextFormField(
+                  textAlign: TextAlign.left,
+                  controller: heightTextController,
+                  keyboardType: TextInputType.number,
+                  inputFormatters: [
+                    FilteringTextInputFormatter.allow(RegExp("[0-9.]")),
+                  ],
+                  decoration: buildInputDecoration(context, "Height (cm)"),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Height cannot be empty';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 15),
+                TextFormField(
+                  controller: weightTextController,
+                  keyboardType: TextInputType.number,
+                  inputFormatters: [
+                    FilteringTextInputFormatter.allow(RegExp("[0-9.]")),
+                  ],
+                  decoration: buildInputDecoration(context, "Weight (kg)"),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Weight cannot be empty';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 15),
+                GenderPicker(
+                  selectedGender: selectedGender,
+                  onChanged: (value) {
+                    setState(() {
+                      selectedGender = value;
+                    });
+                  },
+                  validator: (_) {
+                    if (selectedGender == null) {
+                      return 'Gender must be selected';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 15),
+                ElevatedButton(
+                  onPressed: () {
+                    FocusScope.of(context).unfocus();
+                    final isFormValid =
+                        formKey.currentState?.validate() ?? false;
 
-              if (isFormValid) {
-                logger.i("Saving data...");
-                showSnackBar(context, 'Saving data...');
-              }
-            },
-            child: Text("Submit"),
+                    if (isFormValid) {
+                      logger.i("Saving data...");
+                      showSnackBar(context, 'Saving data...');
+                    }
+                  },
+                  child: Text("Submit"),
+                ),
+              ],
+            ),
           ),
-        ],
+        ),
       ),
     );
   }
