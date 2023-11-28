ab090333cbc2c9657dfc88fec9aab48ff38d2f10
Kevin Lin <kevin.linyun@gmail.com>
2023-11-28 13:06:35 +0800

    part1/19-upcoming-page.md

--- lib/features/upcoming/upcoming_page.dart
+++ lib/features/upcoming/upcoming_page.dart
@@ -1,12 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_learning_path/common/snack_bar.dart';
+import 'package:flutter_learning_path/features/upcoming/upcoming_notifier.dart';
+import 'package:flutter_learning_path/router/routes.dart';
+import 'package:flutter_learning_path/styling/text_styling_extension.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
-class UpcomingPage extends StatelessWidget {
+class UpcomingPage extends ConsumerStatefulWidget {
   const UpcomingPage({Key? key}) : super(key: key);
 
   @override
+  ConsumerState createState() => _UpcomingPageState();
+}
+
+class _UpcomingPageState extends ConsumerState<UpcomingPage> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("Upcoming Page")),
+    final workoutList = ref.watch($upcoming.select((value) => value.workOutList));
+
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      appBar: AppBar(
+        actions: [
+          TextButton(
+            onPressed: () async {
+              await ref.watch($upcoming.notifier).signOut();
+              // need to check mounted because we need to use context inside an async method
+              if (mounted) {
+                context.go(Routes.initial);
+              }
+            },
+            child: Text('Sign out', style: Theme.of(context).textTheme.bodyLarge?.withColorRed),
+          )
+        ],
+        title: Image.asset("assets/images/logo.png", height: 35),
+        backgroundColor: Theme.of(context).colorScheme.background,
+      ),
+      body: ListView.builder(
+        itemCount: workoutList.length,
+        itemBuilder: (context, index) => Text(workoutList[index].toString()),
+      ),
     );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  // Fetch data from backend
+  Future<void> fetchData() async {
+    try {
+      await ref.read($upcoming.notifier).getWorkOut();
+    } catch (e) {
+      // if you want to test the error, deliberately change the path of getWorkOutList in WorkOutRepository to something different to '/workouts-upcoming'
+      if (mounted) {
+        showSnackBar(context, 'Error retrieving workout data', Colors.red);
+      }
+    }
   }
 }
